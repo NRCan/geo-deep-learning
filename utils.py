59b00233571e7598cbf82cac2144d821b25dcd49
E Peterson <epeterson@canada.ca>
2018-10-09 09:19:27 -0400

    Fixed 'info' not defined error in the validate_num_classes exception

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import gdal
 import torch
 from ruamel_yaml import YAML
-from osgeo import gdal, osr, ogr
+from osgeo import gdal, ogr
 
 
 def create_or_empty_folder(folder):
@@ -166,6 +166,6 @@ def validate_num_classes(vector_file, num_classes, value_field):
     source_layer = source_ds.GetLayer()
     name_lyr = source_layer.GetLayerDefn().GetName()
     vector_classes = source_ds.ExecuteSQL("SELECT DISTINCT " + value_field + " FROM " + name_lyr).GetFeatureCount()
-    if vector_classes != num_classes:
+    if vector_classes + 1 != num_classes:
         raise ValueError('The number of classes in the yaml.config (%d) is different than the number of classes in '
-                         'the file %s (%d)' % (num_classes, info['shp'], vector_classes))
+                         'the file %s (%d)' % (num_classes, vector_file, vector_classes))
