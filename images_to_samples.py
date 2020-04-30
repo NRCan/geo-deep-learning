fix and test augmentation.py new functions (RadiometricTrim, RandomCrop, Scale) move radiometric scaling from image_reader_as_array (sample prep) to augmentation.py (training) add source dtype to hdf5 samples add pad() function in utils.py review sampling filters (min_annot_perc and class_proportion) bugfix for visualisation calls in train_segmentation.py

images_to_samples.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+from math import floor, ceil
 import os
 import fiona
 import numpy as np
@@ -12,7 +13,7 @@ from tqdm import tqdm
 from collections import OrderedDict
 
 from utils.CreateDataset import create_files_and_datasets, MetaSegmentationDataset
-from utils.utils import vector_to_raster, get_key_def, lst_ids
+from utils.utils import vector_to_raster, get_key_def, lst_ids, pad, pad_diff
 from utils.readers import read_parameters, image_reader_as_array, read_csv
 from utils.verifications import is_valid_geom, validate_num_classes
 
@@ -53,19 +54,6 @@ def mask_image(arrayA, arrayB):
     return ma_array
 
 
-def pad_diff(arr, w, h, arr_shape):
-    """ Pads img_arr width or height < samples_size with zeros """
-    w_diff = arr_shape - w
-    h_diff = arr_shape - h
-
-    if len(arr.shape) > 2:
-        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff), (0, 0)), "constant", constant_values=(0, 0))
-    else:
-        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff)), "constant", constant_values=(0, 0))
-
-    return padded_arr
-
-
 def append_to_dataset(dataset, sample):
     old_size = dataset.shape[0]  # this function always appends samples on the first axis
     dataset.resize(old_size + 1, axis=0)
@@ -73,7 +61,7 @@ def append_to_dataset(dataset, sample):
     return old_size  # the index to the newly added sample, or the previous size of the dataset
 
 
-def check_sampling_dict():
+def check_sampling_dict():  # TODO replace with get_key_def() ?
     for i, (key, value) in enumerate(params['sample']['sampling'].items()):
 
         if i == 0:
@@ -106,26 +94,32 @@ def minimum_annotated_percent(target_background_percent, min_annotated_percent):
     return False
 
 
-def class_proportion(target):
+def class_proportion(target, sample_size: int, class_min_prop: dict):
     prop_classes = {}
-    sample_total = (params['global']['samples_size']) ** 2
+    sample_total = (sample_size) ** 2
     for i in range(0, params['global']['num_classes'] + 1):
         prop_classes.update({str(i): 0})
-        if i in np.unique(target.flatten()):
-            prop_classes[str(i)] = (round((np.bincount(target.flatten())[i] / sample_total) * 100, 1))
+        if i in np.unique(target.clip(min=0).flatten()):
+            prop_classes[str(i)] = (round((np.bincount(target.clip(min=0).flatten())[i] / sample_total) * 100, 1))
 
-    condition = []
-    for i, (key, value) in enumerate(params['sample']['sampling'].items()):
-        if i >= 2 and prop_classes[key] >= value:
-            condition.append(1)
+    for key, value in class_min_prop.items():
+        if prop_classes[key] < value:
+            return False
 
-    if sum(condition) == (params['global']['num_classes'] + 1):
-        return True
-
-    return False
+    return True
 
 
-def compute_classes(dataset, samples_file, val_percent, val_sample_file, data, target, metadata_idx, dict_classes):
+def compute_classes(dataset,
+                    samples_file,
+                    val_percent,
+                    val_sample_file,
+                    data,
+                    target,
+                    metadata_idx,
+                    dict_classes,
+                    dtype="float32"
+                    ):
+    # TODO: rename this function?
     """ Creates Dataset (trn, val, tst) appended to Hdf5 and computes pixel classes(%) """
     val = False
     if dataset == 'trn':
@@ -136,12 +130,13 @@ def compute_classes(dataset, samples_file, val_percent, val_sample_file, data, t
             val = True
             samples_file = val_sample_file
     append_to_dataset(samples_file["sat_img"], data)
+    append_to_dataset(samples_file["sat_img_dtype"], dtype)
     append_to_dataset(samples_file["map_img"], target)
     append_to_dataset(samples_file["meta_idx"], metadata_idx)
 
     # adds pixel count to pixel_classes dict for each class in the image
     for i in (np.unique(target)):
-        dict_classes[i] += (np.bincount(target.flatten()))[i]
+        dict_classes[i] += (np.bincount(target.clip(min=0).flatten()))[i]
 
     return val
 
@@ -157,12 +152,13 @@ def samples_preparation(in_img_array,
                         val_sample_file,
                         dataset,
                         pixel_classes,
-                        image_metadata=None):
+                        image_metadata=None,
+                        dtype=np.float32):
     """
     Extract and write samples from input image and reference image
     :param in_img_array: numpy array of the input image
     :param label_array: numpy array of the annotation image
-    :param sample_size: (int) Size (in pixel) of the samples to create #FIXME: could there be a different sample size for tst dataset? shows results closer to inference
+    :param sample_size: (int) Size (in pixel) of the samples to create # TODO: could there be a different sample size for tst dataset? shows results closer to inference
     :param overlap: (int) Desired overlap between samples in %
     :param samples_count: (dict) Current number of samples created (will be appended and return)
     :param num_classes: (dict) Number of classes in reference data (will be appended and return)
@@ -196,6 +192,13 @@ def samples_preparation(in_img_array,
     added_samples = 0
     excl_samples = 0
 
+    dontcare = get_key_def("ignore_index", params["training"], -1)  # TODO: deduplicate with train_segmentation, l128
+    if dontcare == 0:
+        warnings.warn("The 'dontcare' value (or 'ignore_index') used in the loss function cannot be zero;"
+                      " all valid class indices should be consecutive, and start at 0. The 'dontcare' value"
+                      " will be remapped to -1 while loading the dataset, and inside the config from now on.")
+        params["training"]["ignore_index"] = -1
+
     with tqdm(range(0, h, dist_samples), position=1, leave=True,
               desc=f'Writing samples to "{dataset}" dataset. Dataset currently contains {idx_samples} '
                    f'samples.') as _tqdm:
@@ -207,69 +210,41 @@ def samples_preparation(in_img_array,
                 data_row = data.shape[0]
                 data_col = data.shape[1]
                 if data_row < sample_size or data_col < sample_size:
-                    data = pad_diff(data, data_row, data_col, sample_size)
+                    h_diff, w_diff = pad_diff(data_row, data_col, sample_size) # array, actual height, actual width, desired size
+                    padding = ((floor(w_diff/2), floor(h_diff/2), ceil(w_diff/2), ceil(h_diff/2))) # left, top, right, bottom
+                    data = pad(data, padding, fill=np.nan)  # don't fill with 0 if possible. Creates false min value when scaling.
 
                 target_row = target.shape[0]
                 target_col = target.shape[1]
                 if target_row < sample_size or target_col < sample_size:
-                    target = pad_diff(target, target_row, target_col, sample_size)
+                    h_diff, w_diff = pad_diff(target_row, target_col,
+                                              sample_size)  # array, actual height, actual width, desired size
+                    padding = ((floor(w_diff/2), floor(h_diff/2), ceil(w_diff/2), ceil(h_diff/2)))
+                    target = pad(target, padding, fill=dontcare)
                 u, count = np.unique(target, return_counts=True)
                 target_background_percent = round(count[0] / np.sum(count) * 100 if 0 in u else 0, 1)
 
-                if len(params['sample']['sampling']['method']) == 1:
-                    if params['sample']['sampling']['method'][0] == 'min_annotated_percent':
-                        if minimum_annotated_percent(target_background_percent, params['sample']['sampling']['map']):
-                            val = compute_classes(dataset, samples_file, val_percent, val_sample_file,
-                                                  data, target, metadata_idx, pixel_classes)
-                            if val:
-                                idx_samples_v += 1
-                            else:
-                                idx_samples += 1
-                                added_samples += 1
-                        else:
-                            excl_samples += 1
+                min_annot_perc = get_key_def('min_annotated_percent', params['sample']['sampling'], 0)
+                class_prop = get_key_def('class_proportion', params['sample']['sampling'], None)
 
-                    if params['sample']['sampling']['method'][0] == 'class_proportion':
-                        if class_proportion(target):
-                            val = compute_classes(dataset, samples_file, val_percent, val_sample_file,
-                                                  data, target, metadata_idx, pixel_classes)
-                            if val:
-                                idx_samples_v += 1
-                            else:
-                                idx_samples += 1
-                                added_samples += 1
-                        else:
-                            excl_samples += 1
-
-                if len(params['sample']['sampling']['method']) == 2:
-                    if params['sample']['sampling']['method'][0] == 'min_annotated_percent':
-                        if minimum_annotated_percent(target_background_percent, params['sample']['sampling']['map']):
-                            if params['sample']['sampling']['method'][1] == 'class_proportion':
-                                if class_proportion(target):
-                                    val = compute_classes(dataset, samples_file, val_percent, val_sample_file,
-                                                          data, target, metadata_idx, pixel_classes)
-                                    if val:
-                                        idx_samples_v += 1
-                                    else:
-                                        idx_samples += 1
-                                        added_samples += 1
-                                else:
-                                    excl_samples += 1
-
-                    elif params['sample']['sampling']['method'][0] == 'class_proportion':
-                        if class_proportion(target):
-                            if params['sample']['sampling']['method'][1] == 'min_annotated_percent':
-                                if minimum_annotated_percent(target_background_percent,
-                                                             params['sample']['sampling']['map']):
-                                    val = compute_classes(dataset, samples_file, val_percent, val_sample_file,
-                                                          data, target, metadata_idx, pixel_classes)
-                                    if val:
-                                        idx_samples_v += 1
-                                    else:
-                                        idx_samples += 1
-                                        added_samples += 1
-                                else:
-                                    excl_samples += 1
+                if minimum_annotated_percent(target_background_percent, min_annot_perc) and \
+                        class_proportion(target, sample_size, class_prop):
+                    val = compute_classes(dataset=dataset,
+                                          samples_file=samples_file,
+                                          val_percent=val_percent,
+                                          val_sample_file=val_sample_file,
+                                          data=data,
+                                          target=target,
+                                          metadata_idx=metadata_idx,
+                                          dict_classes=pixel_classes,
+                                          dtype=dtype)
+                    if val:
+                        idx_samples_v += 1
+                    else:
+                        idx_samples += 1
+                        added_samples += 1
+                else:
+                    excl_samples += 1
 
                 target_class_num = np.max(u)
                 if num_classes < target_class_num:
@@ -307,7 +282,6 @@ def main(params):
     val_percent = params['sample']['val_percent']
     samples_size = params["global"]["samples_size"]
     overlap = params["sample"]["overlap"]
-    min_annot_perc = params['sample']['sampling']['map']
     num_bands = params['global']['number_of_bands']
     debug = get_key_def('debug_mode', params['global'], False)
     if debug:
@@ -323,18 +297,18 @@ def main(params):
             final_samples_folder = os.path.join(data_path, "samples")
         else:
             final_samples_folder = "samples"
-        samples_folder = f'samples{samples_size}_overlap{overlap}_min-annot{min_annot_perc}_{num_bands}bands'  # TODO: validate this is preferred name structure
+        samples_folder = f'samples{samples_size}_overlap{overlap}_{num_bands}bands'  # TODO: validate this is preferred name structure
 
     else:
         list_data_prep = read_csv(csv_file)
-        samples_folder = data_path.joinpath(f'samples{samples_size}_overlap{overlap}_min-annot{min_annot_perc}_{num_bands}bands')
+        samples_folder = data_path.joinpath(f'samples{samples_size}_overlap{overlap}_{num_bands}bands')
 
     if samples_folder.is_dir():
         warnings.warn(f'Data path exists: {samples_folder}. Suffix will be added to directory name.')
         samples_folder = Path(str(samples_folder) + '_' + now)
     else:
         tqdm.write(f'Writing samples to {samples_folder}')
-    Path.mkdir(samples_folder, exist_ok=False)  # FIXME: what if we want to append samples to existing hdf5?
+    Path.mkdir(samples_folder, exist_ok=False)  # TODO: what if we want to append samples to existing hdf5?
     tqdm.write(f'Samples will be written to {samples_folder}\n\n')
 
     tqdm.write(f'\nSuccessfully read csv file: {Path(csv_file).stem}\nNumber of rows: {len(list_data_prep)}\nCopying first entry:\n{list_data_prep[0]}\n')
@@ -371,7 +345,7 @@ def main(params):
     number_classes = 0
 
     # 'sampling' ordereddict validation
-    check_sampling_dict()
+    # check_sampling_dict() # TODO replace with get_key_def() ?
 
     pixel_classes = {}
     # creates pixel_classes dict and keys
@@ -401,15 +375,15 @@ def main(params):
                         info['meta'] = info['meta'].split('/')[-1]
 
                 with rasterio.open(info['tif'], 'r') as raster:
-                    # Burn vector file in a raster file
+                    # 1. Burn vector file in a raster file
                     np_label_raster = vector_to_raster(vector_file=info['gpkg'],
                                                        input_image=raster,
                                                        attribute_name=info['attribute_name'],
                                                        fill=get_key_def('ignore_idx',
                                                                         get_key_def('training', params, {}), 0))
-                    # Read the input raster image
+                    # 2. Read the input raster image
+                    dtype = raster.meta["dtype"]
                     np_input_image = image_reader_as_array(input_image=raster,
-                                                           scale=get_key_def('scale_data', params['global'], None),
                                                            aux_vector_file=get_key_def('aux_vector_file',
                                                                                        params['global'], None),
                                                            aux_vector_attrib=get_key_def('aux_vector_attrib',
@@ -448,22 +422,24 @@ def main(params):
                     f"'number_of_bands' in the yaml file ({params['global']['number_of_bands']}) should be identical"
 
                 np_label_raster = np.reshape(np_label_raster, (np_label_raster.shape[0], np_label_raster.shape[1], 1))
-                number_samples, number_classes = samples_preparation(np_input_image,
-                                                                     np_label_raster,
-                                                                     samples_size,
-                                                                     overlap,
-                                                                     number_samples,
-                                                                     number_classes,
-                                                                     out_file,
-                                                                     val_percent,
-                                                                     val_file,
-                                                                     info['dataset'],
-                                                                     pixel_classes,
-                                                                     metadata)
+                # 3. Prepare samples!
+                number_samples, number_classes = samples_preparation(in_img_array=np_input_image,
+                                                                     label_array=np_label_raster,
+                                                                     sample_size=samples_size,
+                                                                     overlap=overlap,
+                                                                     samples_count=number_samples,
+                                                                     num_classes=number_classes,
+                                                                     samples_file=out_file,
+                                                                     val_percent=val_percent,
+                                                                     val_sample_file=val_file,
+                                                                     dataset=info['dataset'],
+                                                                     pixel_classes=pixel_classes,
+                                                                     image_metadata=metadata,
+                                                                     dtype=dtype)
 
                 _tqdm.set_postfix(OrderedDict(number_samples=number_samples))
                 out_file.flush()
-            except Exception as e:
+            except IOError as e:
                 warnings.warn(f'An error occurred while preparing samples with "{Path(info["tif"]).stem}" (tiff) and '
                               f'{Path(info["gpkg"]).stem} (gpkg). Error: "{e}"')
                 continue
@@ -479,7 +455,8 @@ def main(params):
 
     # prints the proportion of pixels of each class for the samples created
     for i in pixel_classes:
-        print('Pixels from class', i, ':', round((pixel_classes[i] / pixel_total) * 100, 1), '%')
+        prop = round((pixel_classes[i] / pixel_total) * 100, 1) if pixel_total > 0 else 0
+        print('Pixels from class', i, ':', prop, '%')
 
     print("Number of samples created: ", number_samples)
 

inference.py
@@ -18,6 +18,7 @@ from tqdm import tqdm
 from pathlib import Path
 
 from models.model_choice import net
+from utils.augmentation import Scale
 from utils.utils import load_from_checkpoint, get_device_ids, gpu_stats, get_key_def
 from utils.readers import read_parameters, image_reader_as_array, read_csv
 from utils.CreateDataset import MetaSegmentationDataset
@@ -29,7 +30,18 @@ except ModuleNotFoundError:
     pass
 
 
-def sem_seg_inference(model, nd_array, overlay, chunk_size, num_classes, device, meta_map=None, metadata=None, output_path=Path(os.getcwd()), index=0, debug=False):
+def sem_seg_inference(model,
+                      nd_array,
+                      overlay,
+                      chunk_size,
+                      scale,
+                      num_classes,
+                      device,
+                      meta_map=None,
+                      metadata=None,
+                      output_path=Path(os.getcwd()),
+                      index=0,
+                      debug=False):
     """Inference on images using semantic segmentation
     Args:
         model: model to use for inference
@@ -81,9 +93,12 @@ def sem_seg_inference(model, nd_array, overlay, chunk_size, num_classes, device,
                         chunk_input = padded_array[row_start:row_end, col_start:col_end, :]
                         if meta_map:
                             chunk_input = MetaSegmentationDataset.append_meta_layers(chunk_input, meta_map, metadata)
+                        scale_transform = Scale(scale)
+                        chunk_input = scale_transform(chunk_input) # FIXME: test this!!!
+
                         inputs = torch.from_numpy(np.float32(np.transpose(chunk_input, (2, 0, 1))))
 
-                        inputs.unsqueeze_(0) #Add dummy batch dimension
+                        inputs.unsqueeze_(0)  # Add dummy batch dimension
 
                         inputs = inputs.to(device)
                         # forward
@@ -310,11 +325,9 @@ def main(params):
 
                 assert local_img.is_file(), f"Could not open raster file at {local_img}"
 
-                scale = get_key_def('scale_data', params['global'], None)
                 with rasterio.open(local_img, 'r') as raster:
 
                     np_input_image = image_reader_as_array(input_image=raster,
-                                                           scale=scale,
                                                            aux_vector_file=get_key_def('aux_vector_file',
                                                                                        params['global'], None),
                                                            aux_vector_attrib=get_key_def('aux_vector_attrib',
@@ -351,9 +364,20 @@ def main(params):
                                   f"can not be larger than the number of band in the input image ({input_band_count}).")
                     continue
 
+                scale = get_key_def('scale_data', params['global'], None)
                 # START INFERENCES ON SUB-IMAGES
-                sem_seg_results_per_class = sem_seg_inference(model, np_input_image, nbr_pix_overlap, chunk_size, num_classes_corrected,
-                                                    device, meta_map, metadata, output_path=working_folder, index=_tqdm.n, debug=debug)
+                sem_seg_results_per_class = sem_seg_inference(model,
+                                                              np_input_image,
+                                                              nbr_pix_overlap,
+                                                              chunk_size,
+                                                              scale,
+                                                              num_classes_corrected,
+                                                              device,
+                                                              meta_map,
+                                                              metadata,
+                                                              output_path=working_folder,
+                                                              index=_tqdm.n,
+                                                              debug=debug)
 
                 # CREATE GEOTIF FROM METADATA OF ORIGINAL IMAGE
                 tqdm.write(f'Saving inference...\n')

utils/CreateDataset.py
@@ -31,9 +31,10 @@ def create_files_and_datasets(params, samples_folder):
                                  maxshape=(None, samples_size, samples_size))
         hdf5_file.create_dataset("meta_idx", (0, 1), dtype=np.int16, maxshape=(None, 1))
         try:
+            hdf5_file.create_dataset("sat_img_dtype", shape=(0, 1), dtype=h5py.string_dtype(), maxshape=(None, 1))
             hdf5_file.create_dataset("metadata", (0, 1), dtype=h5py.string_dtype(), maxshape=(None, 1))
         except AttributeError as e:
-            warnings.warn(f'Ignoring error: {e}. Make sure no metadata is used or update h5py to version 2.10 or higher')
+            raise (f'{e}. Update h5py to version 2.10 or higher')
         hdf5_files.append(hdf5_file)
     return hdf5_files
 
@@ -70,7 +71,8 @@ class SegmentationDataset(Dataset):
         return self.max_sample_count
 
     def _remap_labels(self, map_img):
-        # note: will do nothing if 'dontcare' is not set in constructor, or set to non-zero value
+        map_img = np.nan_to_num(map_img, copy=False, nan=self.dontcare)  # FIXME: test this.
+        # note: will do nothing if 'dontcare' is not set in constructor, or set to non-zero value # TODO: seems like a temporary patch... dontcare should never be == 0, right ?
         if self.dontcare is None or self.dontcare != 0:
             return map_img
         # for now, the current implementation only handles the original 'dontcare' value as zero
@@ -87,7 +89,9 @@ class SegmentationDataset(Dataset):
             metadata = None
             if meta_idx != -1:
                 metadata = self.metadata[meta_idx]
-        sample = {"sat_img": sat_img, "map_img": map_img, "metadata": metadata, "hdf5_path": self.hdf5_path}
+            sat_img_dtype = hdf5_file["sat_img_dtype"][index, ...][0]
+        sample = {"sat_img": sat_img, "map_img": map_img, "metadata": metadata,
+                  "sat_img_dtype": sat_img_dtype, "hdf5_path": self.hdf5_path}
         if self.radiom_transform:  # radiometric transforms should always precede geometric ones
             sample = self.radiom_transform(sample)
         if self.geom_transform:  # rotation, geometric scaling, flip and crop. Will also put channels first and convert to torch tensor from numpy.
@@ -149,6 +153,6 @@ class MetaSegmentationDataset(SegmentationDataset):
         if self.radiom_transform:  # radiometric transforms should always precede geometric ones
             sample = self.radiom_transform(sample)  # FIXME: test this
         sample["sat_img"] = self.append_meta_layers(sat_img, self.meta_map, self.metadata[meta_idx])  # Overwrite sat_img with sat_img with metalayers
-        if self.geom_transform:
+        if self.geom_transform:  # TODO: geom transform should always be True as it includes ToTensorTarget.
             sample = self.geom_transform(sample)  # rotation, geometric scaling, flip and crop. Will also put channels first and convert to torch tensor from numpy.
         return sample

utils/augmentation.py
@@ -1,6 +1,9 @@
 # WARNING: data being augmented may be scaled to (0,1) rather, for example, (0,255). Therefore, implementing radiometric
 # augmentations (ex.: changing hue, saturation, brightness, contrast) may give undesired results.
 # Scaling process is done in images_to_samples.py l.215
+import numbers
+import warnings
+from typing import Sequence
 
 import torch
 # import torch should be first. Unclear issue, mentioned here: https://github.com/pytorch/pytorch/issues/2083
@@ -9,10 +12,10 @@ import numpy as np
 from skimage import transform, exposure
 from torchvision import transforms
 
-from utils.utils import get_key_def
+from utils.utils import get_key_def, pad, minmax_scale, pad_diff
 
 
-def compose_transforms(params, dataset, type=''):
+def compose_transforms(params, dataset, type='', ignore_index=None): # FIXME: how to get ignroe index for randomcrop?
     """
     Function to compose the transformations to be applied on every batches.
     :param params: (dict) Parameters found in the yaml config file
@@ -27,6 +30,7 @@ def compose_transforms(params, dataset, type=''):
             radiom_trim_range = get_key_def('radiom_trim_range', params['training']['augmentation'], None)
             brightness_contrast_range = get_key_def('brightness_contrast_range', params['training']['augmentation'], None)
             noise = get_key_def('noise', params['training']['augmentation'], None)
+            scale = get_key_def('scale_data', params['global'], None)
 
             if radiom_trim_range:  # Contrast stretching
                 lst_trans.append(RadiometricTrim(range=radiom_trim_range))  # FIXME: test this. Assure compatibility with CRIM devs (don't trim metadata)
@@ -39,6 +43,9 @@ def compose_transforms(params, dataset, type=''):
                 # lst_trans.append()
                 pass
 
+            if scale:
+                lst_trans.append(Scale(scale))
+
         elif type == 'geometric':
             geom_scale_range = get_key_def('geom_scale_range', params['training']['augmentation'], None)
             hflip = get_key_def('hflip_prob', params['training']['augmentation'], None)
@@ -47,7 +54,7 @@ def compose_transforms(params, dataset, type=''):
             crop_size = get_key_def('crop_size', params['training']['augmentation'], None)
 
             if geom_scale_range:
-                lst_trans.append(GeometricScale(range=geom_scale_range))  # FIXME: test this
+                lst_trans.append(GeometricScale(range=geom_scale_range))
 
             if hflip:
                 lst_trans.append(HorizontalFlip(prob=params['training']['augmentation']['hflip_prob']))
@@ -56,11 +63,14 @@ def compose_transforms(params, dataset, type=''):
                 lst_trans.append(RandomRotationTarget(limit=rotate_limit, prob=rotate_prob))
 
             if crop_size:
-                lst_trans.append(RandomCrop(sample_size=crop_size))  # FIXME: test this
+                lst_trans.append(RandomCrop(sample_size=crop_size, ignore_index=ignore_index))
 
             lst_trans.append(ToTensorTarget())  # Send channels first, convert numpy array to torch tensor
 
     else:
+        scale = get_key_def('scale_data', params['global'], None)
+        if scale:
+            lst_trans.append(Scale(scale))
         lst_trans.append(ToTensorTarget())  # Send channels first, convert numpy array to torch tensor
 
     return transforms.Compose(lst_trans)
@@ -72,10 +82,54 @@ class RadiometricTrim(object):
         self.range = range
 
     def __call__(self, sample):
-        trim = round(random.uniform(range[0], range[-1]), 1)
-        perc_left, perc_right = np.percentile(trim, 100-trim)
-        sat_img = exposure.rescale_intensity(sample['sat_img'], in_range=(perc_left, perc_right))
-        return {'sat_img': sat_img, 'map_img': sample['map_img']}
+        trim = round(random.uniform(self.range[0], self.range[-1]), 1)
+        out_dtype = sample['sat_img_dtype']
+        rescaled_sat_img = np.empty(sample['sat_img'].shape, dtype=sample['sat_img'].dtype)
+        for band_idx in range(sample['sat_img'].shape[2]):
+            band = sample['sat_img'][:, :, band_idx]
+            perc_left, perc_right = np.nanpercentile(band, (trim, 100-trim))
+            rescaled_band = exposure.rescale_intensity(band, in_range=(perc_left, perc_right), out_range=out_dtype)
+            rescaled_sat_img[:, :, band_idx] = rescaled_band
+        sample['sat_img'] = rescaled_sat_img
+        return sample
+
+
+class Scale(object):
+    """
+    Scale array values from range [0,255]  or [0,65535] to values in config (e.g. [0,1])
+    Guidelines for pre-processing: http://cs231n.github.io/neural-networks-2/#datapre
+    """
+    def __init__(self, range):
+        if isinstance(range, Sequence) and len(range) == 2:
+            self.sc_min = range[0]
+            self.sc_max = range[1]
+        else:
+            raise TypeError('Got inappropriate scale arg')
+
+    def __call__(self, sample):
+        """
+        Args:
+            sample (ndarray): Image to be scaled.
+
+        Returns:
+            ndarray: Scaled image.
+        """
+        min_val, max_val = np.nanmin(sample['sat_img']), np.nanmax(sample['sat_img'])
+        out_dtype = sample['sat_img_dtype']
+        if 'int' in out_dtype:
+            orig_range = (np.iinfo(out_dtype).min, np.iinfo(out_dtype).max)
+        elif min_val >= 0 and max_val <= 65535:
+            orig_range = (0, 65535)
+            warnings.warn(f"Values in input image of shape {sample['sat_img'].shape} "
+                          f"range from {min_val} to {max_val}."
+                          f"Image will be considered 16 bit for scaling.")
+        else:
+            raise ValueError(f"Invalid values in input image. They should range from 0 to 255 or 65535, not"
+                             f"{min_val} to {max_val}.")
+
+        sample['sat_img'] = minmax_scale(img=sample['sat_img'], orig_range=orig_range, scale_range=(self.sc_min, self.sc_max))
+
+        return sample
 
 
 class GeometricScale(object):
@@ -89,7 +143,9 @@ class GeometricScale(object):
         output_height =  sample['sat_img'].shape[1] * scale_factor
         sat_img = transform.resize(sample['sat_img'], output_shape=(output_height, output_width))
         map_img = transform.resize(sample['map_img'], output_shape=(output_height, output_width))
-        return {'sat_img': sat_img, 'map_img': map_img}
+        sample['sat_img'] = sat_img
+        sample['map_img'] = map_img
+        return sample
 
 
 class RandomRotationTarget(object):
@@ -103,7 +159,9 @@ class RandomRotationTarget(object):
             angle = np.random.uniform(-self.limit, self.limit)
             sat_img = transform.rotate(sample['sat_img'], angle, preserve_range=True)
             map_img = transform.rotate(sample['map_img'], angle, preserve_range=True)
-            return {'sat_img': sat_img, 'map_img': map_img}
+            sample['sat_img'] = sat_img
+            sample['map_img'] = map_img
+            return sample
         else:
             return sample
 
@@ -117,36 +175,98 @@ class HorizontalFlip(object):
         if random.random() < self.prob:
             sat_img = np.ascontiguousarray(sample['sat_img'][:, ::-1, ...])
             map_img = np.ascontiguousarray(sample['map_img'][:, ::-1, ...])
-            return {'sat_img': sat_img, 'map_img': map_img}
+            sample['sat_img'] = sat_img
+            sample['map_img'] = map_img
+            return sample
         else:
             return sample
 
 
-class RandomCrop(object):  # FIXME: delete overlap in samples_prep (images_to_samples, l.106)
-    """Randomly crop image according to a certain dimension."""
-    def __init__(self, sample_size):
-        self.sample_size = sample_size
+class RandomCrop(object):  # TODO: what to do with overlap in samples_prep (images_to_samples, l.106)? overlap doesn't need to be larger than, say, 5%
+    """Randomly crop image according to a certain dimension.
+    Adapted from https://pytorch.org/docs/stable/_modules/torchvision/transforms/transforms.html#RandomCrop
+    to support >3 band images (not currently supported by PIL)"""
+    def __init__(self, sample_size, padding=3, pad_if_needed=True, ignore_index=0):
+        if isinstance(sample_size, numbers.Number):
+            self.size = (int(sample_size), int(sample_size))
+        else:
+            self.size = sample_size
+        self.padding = padding
+        self.pad_if_needed = pad_if_needed
+        self.ignore_index = ignore_index
 
-    def __call__(self, sample):
-        ########################################################################
-        # select a (sample_size x sample_size) random crop from the img and label:
-        ########################################################################
-        start_x = np.random.randint(low=0, high=(sample['sat_img'].shape[0] - self.sample_size))
-        end_x = start_x + self.sample_size
-        start_y = np.random.randint(low=0, high=(sample['sat_img'].shape[1] - self.sample_size))
-        end_y = start_y + self.sample_size
+    @staticmethod
+    def get_params(img, output_size):
+        """Get parameters for ``crop`` for a random crop.
 
-        sat_img = sample['sat_img'][start_y:end_y, start_x:end_x]  # ex.: (shape: (256, 256, 3))
-        map_img = sample['map_img'][start_y:end_y, start_x:end_x]  # ex.: (shape: (256, 256))
-        return {'sat_img': sat_img, 'map_img': map_img}
+        Args:
+            img (ndarray): Image to be cropped.
+            output_size (tuple): Expected output size of the crop.
+
+        Returns:
+            tuple: params (i, j, h, w) to be passed to ``crop`` for random crop.
+        """
+        h, w = img.shape[:2]
+        th, tw = output_size
+        if w == tw and h == th:
+            return 0, 0, h, w
+
+        i = random.randint(0, h - th)
+        j = random.randint(0, w - tw)
+        return i, j, th, tw
+
+    def __call__(self, sample):  # TODO: test this!!!
+        """
+        Args:
+            sample (ndarray): Image to be cropped.
+
+        Returns:
+            ndarray: Cropped image.
+        """
+        sat_img = sample['sat_img']
+        map_img = sample['map_img']
+
+        if self.padding is not None:
+            sat_img = pad(sat_img, self.padding, np.nan)  # Pad with nan values for sat_img
+            map_img = pad(map_img, self.padding, self.ignore_index)  # Pad with dontcare values for map_img
+
+        # pad the height if needed
+        if self.pad_if_needed and sat_img.shape[0] < self.size[0]:
+            sat_img = pad(sat_img, (0, self.size[0] - sat_img.shape[0]), np.nan)
+        # pad the width if needed
+        if self.pad_if_needed and sat_img.shape[1] < self.size[1]:
+            sample = pad(sat_img, (self.size[1] - sat_img.shape[1], 0), np.nan)
+
+        # pad the height if needed
+        if self.pad_if_needed and map_img.shape[0] < self.size[0]:
+            map_img = pad(map_img, (0, self.size[0] - map_img.shape[0]), self.ignore_index)
+        # pad the width if needed
+        if self.pad_if_needed and map_img.shape[1] < self.size[1]:
+            map_img = pad(map_img, (self.size[1] - map_img.shape[1], 0), self.ignore_index)
+
+        i, j, h, w = self.get_params(sat_img, self.size)
+
+        sat_img = sat_img[i:i + h, j:j + w]
+        map_img = map_img[i:i + h, j:j + w]
+
+        sample['sat_img'] = sat_img
+        sample['map_img'] = map_img
+        return sample
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0}, padding={1})'.format(self.size, self.padding)
 
 
 class ToTensorTarget(object):
     """Convert ndarrays in sample to Tensors."""
 
     def __call__(self, sample):
-        # FIXME: add default trim (e.g. 0.1% or 0.5%)
-        #  FIXME: scale between 0 and 1 HERE, not in samples_prep.
-        sat_img = np.float32(np.transpose(sample['sat_img'], (2, 0, 1)))
+        sat_img = np.nan_to_num(sample['sat_img'], copy=False, nan=0.0)
+        sat_img = np.float32(np.transpose(sat_img, (2, 0, 1)))
+        sat_img = torch.from_numpy(sat_img)
+
         map_img = np.int64(sample['map_img'])
-        return {'sat_img': torch.from_numpy(sat_img), 'map_img': torch.from_numpy(map_img)}
+        map_img = torch.from_numpy(map_img)
+        sample['sat_img'] = sat_img
+        sample['map_img'] = map_img
+        return sample

utils/readers.py
@@ -5,7 +5,7 @@ from ruamel_yaml import YAML
 from tqdm import tqdm
 from pathlib import Path
 
-from utils.utils import vector_to_raster, minmax_scale
+from utils.utils import vector_to_raster
 
 
 def read_parameters(param_file):
@@ -21,7 +21,7 @@ def read_parameters(param_file):
     return params
 
 
-def image_reader_as_array(input_image, scale=None, aux_vector_file=None, aux_vector_attrib=None, aux_vector_ids=None,
+def image_reader_as_array(input_image, aux_vector_file=None, aux_vector_attrib=None, aux_vector_ids=None,
                           aux_vector_dist_maps=False, aux_vector_dist_log=True, aux_vector_scale=None):
     """Read an image from a file and return a 3d array (h,w,c)
     Args:
@@ -40,17 +40,6 @@ def image_reader_as_array(input_image, scale=None, aux_vector_file=None, aux_vec
     np_array = np.empty([input_image.height, input_image.width, input_image.count], dtype=np.float32)
     for i in tqdm(range(input_image.count), position=1, leave=False, desc=f'Reading image bands: {Path(input_image.files[0]).stem}'):
         np_array[:, :, i] = input_image.read(i+1)  # Bands starts at 1 in rasterio not 0  # TODO: reading a large image >10Gb is VERY slow. Is this line the culprit?
-
-    # Guidelines for pre-processing: http://cs231n.github.io/neural-networks-2/#datapre
-    # Scale array values from range [0,255] to values in config (e.g. [0,1])
-    if scale:
-        sc_min, sc_max = scale
-        assert np.min(np_array) >= 0 and np.max(np_array) <= 255, f'Values in input image of shape {np_array.shape} ' \
-                                                                  f'range from {np.min(np_array)} to {np.max(np_array)}.' \
-                                                                  f'They should range from 0 to 255 (8bit).'
-        np_array = minmax_scale(img=np_array,
-                                orig_range=(0, 255),
-                                scale_range=(sc_min, sc_max))
 
     # if requested, load vectors from external file, rasterize, and append distance maps to array
     if aux_vector_file is not None:

utils/utils.py
@@ -1,3 +1,6 @@
+import numbers
+from typing import Sequence
+
 import torch
 # import torch should be first. Unclear issue, mentioned here: https://github.com/pytorch/pytorch/issues/2083
 from torch import nn
@@ -117,14 +120,6 @@ def image_reader_as_array(input_image, scale=None, aux_vector_file=None, aux_vec
     np_array = np.empty([input_image.height, input_image.width, input_image.count], dtype=np.float32)
     for i in range(input_image.count):
         np_array[:, :, i] = input_image.read(i+1)  # Bands starts at 1 in rasterio not 0
-
-    # Guidelines for pre-processing: http://cs231n.github.io/neural-networks-2/#datapre
-    # Scale arrays to values [0,1]. Default: will scale. Useful if dealing with 8 bit *and* 16 bit images.
-    if scale:
-        sc_min, sc_max = scale
-        np_array = minmax_scale(img=np_array,
-                                orig_range=(np.min(np_array), np.max(np_array)),
-                                scale_range=(sc_min, sc_max))
 
     # if requested, load vectors from external file, rasterize, and append distance maps to array
     if aux_vector_file is not None:
@@ -376,3 +371,57 @@ def create_new_raster_from_base(input_raster, output_raster, write_array):
                 dst.write(write_array[:, :], 1)
             elif count == 3:
                 dst.write(write_array[:, :, :3])  # Take only first three bands assuming they are RGB.
+
+
+def pad(img, padding, fill=0):
+    r"""Pad the given ndarray on all sides with specified padding mode and fill value.
+    Adapted from https://github.com/pytorch/vision/blob/master/torchvision/transforms/functional.py#L255
+    Args:
+        img (ndarray): Image to be padded.
+        padding (int or tuple): Padding on each border. If a single int is provided this
+            is used to pad all borders. If tuple of length 2 is provided this is the padding
+            on left/right and top/bottom respectively. If a tuple of length 4 is provided
+            this is the padding for the left, top, right and bottom borders
+            respectively.
+        fill: Pixel fill value for constant fill. Default is 0. If a tuple of
+            length 3, it is used to fill R, G, B channels respectively.
+            This value is only used when the padding_mode is constant
+    Returns:
+        ndarray: Padded image.
+    """
+    if not isinstance(padding, (numbers.Number, tuple)):
+        raise TypeError('Got inappropriate padding arg')
+    if not isinstance(fill, (numbers.Number, str, tuple)):
+        raise TypeError('Got inappropriate fill arg')
+
+    if isinstance(padding, Sequence) and len(padding) not in [2, 4]:
+        raise ValueError("Padding must be an int or a 2, or 4 element tuple, not a " +
+                         "{} element tuple".format(len(padding)))
+
+    if isinstance(padding, int):
+        pad_left = pad_right = pad_top = pad_bottom = padding
+    if isinstance(padding, Sequence) and len(padding) == 2:
+        pad_left = pad_right = padding[0]
+        pad_top = pad_bottom = padding[1]
+    if isinstance(padding, Sequence) and len(padding) == 4:
+        pad_left = padding[0]
+        pad_top = padding[1]
+        pad_right = padding[2]
+        pad_bottom = padding[3]
+
+    # RGB image
+    if len(img.shape) == 3:
+        img = np.pad(img, ((pad_top, pad_bottom), (pad_left, pad_right), (0, 0)), constant_values=fill)
+    # Grayscale image
+    elif len(img.shape) == 2:
+        img = np.pad(img, ((pad_top, pad_bottom), (pad_left, pad_right)), constant_values=fill)
+
+    return img
+
+
+def pad_diff(actual_height, actual_width, desired_shape):
+    """ Pads img_arr width or height < samples_size with zeros """
+    h_diff = desired_shape - actual_height
+    w_diff = desired_shape - actual_width
+
+    return h_diff, w_diff