f7c06f00a9ed16445dde8132dc21fa4d112160bd
valhassan <victor.alhassan@canada.ca>
2021-07-21 13:19:34 -0400

    - target_size modified to solve out of memory issues

--- gdl_hyperopt_template.py
+++ gdl_hyperopt_template.py
@@ -22,7 +22,7 @@ from utils.readers import read_parameters
 from train_segmentation import main as train_main
 
 # This is the hyperparameter space to explore
-my_space = {'target_size': hp.choice('target_size', [512, 896]),
+my_space = {'target_size': hp.choice('target_size', [512, 636]),
             'model_name': hp.choice('model_name', ['unet_pretrained', 'deeplabv3_resnet101']),
             'loss_fn': hp.choice('loss_fn', ['CrossEntropy', 'Lovasz', 'Duo']),
             'optimizer': hp.choice('optimizer', ['adam', 'adabound']),
