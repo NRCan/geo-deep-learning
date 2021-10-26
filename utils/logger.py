0587faeec0e29fd37036aeaf94d00db6ca051bf2
remtav <34774759+remtav@users.noreply.github.com>
2021-10-26 12:17:51 -0400

    minor changes to logging

--- utils/logger.py
+++ utils/logger.py
@@ -55,7 +55,7 @@ def save_logs_to_bucket(bucket, bucket_output_path, output_path, now, batch_metr
     bucket.upload_file("output.txt", os.path.join(bucket_output_path, f"Logs/{now}_output.txt"))
 
 
-def set_logging(console_level: str = 'WARNING', logfiles_dir=[str, Path], logfiles_prefix: str = 'log',
+def set_logging(console_level: str = 'WARNING', logfiles_dir: Union[str, Path] = None, logfiles_prefix: str = 'log',
                 conf_path: Union[str, Path] = 'utils/logging.conf'):
     """
     Configures logging with provided ".conf" file, console level, output paths.
@@ -64,6 +64,15 @@ def set_logging(console_level: str = 'WARNING', logfiles_dir=[str, Path], logfil
     @param logfiles_dir: path where output logs will be written
     @return:
     """
+    root = logging.getLogger()
+    if root.handlers:
+        for handler in root.handlers:
+            root.removeHandler(handler)
+    if not logfiles_dir:
+        logging.basicConfig(format='%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s',
+                            datefmt='%m/%d/%Y %I:%M:%S %p',
+                            level=logging.INFO)
+        return
     conf_path = Path(conf_path).absolute()
     if not conf_path.is_file():
         raise FileNotFoundError(f'Invalid logging configuration file')
