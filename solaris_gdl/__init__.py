ff6e20e413e23c90729a00fa9086c5c2790e58b9
remtav <34774759+remtav@users.noreply.github.com>
2022-01-20 15:10:18 -0500

    add solaris utilities useful to GDL. Integrates bugfixes and features developped in the last months. Removes gdal dependency

---
+++ solaris_gdl/__init__.py
@@ -0,0 +1,5 @@
+# This code has been onboarded from the solaris project:
+# Source: https://github.com/CosmiQ/solaris
+# License: https://github.com/CosmiQ/solaris/blob/master/LICENSE.txt
+# Thanks to all developers and contributors that have worked over the years to make solaris as useful as it is.
+from . import eval, raster, tile, utils, vector
