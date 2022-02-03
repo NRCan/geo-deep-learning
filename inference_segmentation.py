873cc4d19368cdeef7eb888430a572102888e217
valhassa <victor.alhassan@canada.ca>
2022-02-03 14:27:30 -0500

    - fixed SingleClassCriterion func in __init__.py - minor changes in default_training.yaml and default_visualization.yaml to support singleclass mode and visualization - added singleclass mode in inference_segmentation.py - fix for singleclass mode im metrics.py - added singleclass mode in train_segmentation.py and minor fixes for visualization - bug fix in verifications.py - added singlclass mode in visualization.py

--- inference_segmentation.py
+++ inference_segmentation.py
@@ -52,9 +52,9 @@ def _pad_diff(arr, w, h, arr_shape):
     h_diff = arr_shape - h
 
     if len(arr.shape) > 2:
-        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff), (0, 0)), "constant", constant_values=np.nan)
+        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff), (0, 0)), mode="constant", constant_values=np.nan)
     else:
-        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff)), "constant", constant_values=np.nan)
+        padded_arr = np.pad(arr, ((0, w_diff), (0, h_diff)), mode="constant", constant_values=np.nan)
 
     return padded_arr
 
@@ -63,9 +63,9 @@ def _pad(arr, chunk_size):
     """ Pads img_arr """
     aug = int(round(chunk_size * (1 - 1.0 / 2.0)))
     if len(arr.shape) > 2:
-        padded_arr = np.pad(arr, ((aug, aug), (aug, aug), (0, 0)), mode='reflect')
+        padded_arr = np.pad(arr, ((aug, aug), (aug, aug), (0, 0)), mode="reflect")
     else:
-        padded_arr = np.pad(arr, ((aug, aug), (aug, aug)), mode='reflect')
+        padded_arr = np.pad(arr, ((aug, aug), (aug, aug)), mode="reflect")
 
     return padded_arr
 
@@ -211,6 +211,10 @@ def segmentation(param,
     img_gen = gen_img_samples(src=input_image,
                               chunk_size=chunk_size,
                               step=step)
+    single_class_mode = True
+    threshold = 0.5
+    if num_classes > 1:
+        single_class_mode = False
     start_seg = time.time()
     print_log = True
     for img in tqdm(img_gen, position=1, leave=False,
@@ -263,11 +267,16 @@ def segmentation(param,
             logging.debug(f'Shape of augmented output: {augmented_output.shape}')
             # reverse augmentation for outputs
             deaugmented_output = transformer.deaugment_mask(augmented_output)
-            deaugmented_output = F.softmax(deaugmented_output, dim=1).squeeze(dim=0)
+            if single_class_mode:
+                deaugmented_output = deaugmented_output.squeeze(dim=0)
+            else:
+                deaugmented_output = F.softmax(deaugmented_output, dim=1).squeeze(dim=0)
             output_lst.append(deaugmented_output)
         outputs = torch.stack(output_lst)
         outputs = torch.mul(outputs, WINDOW_SPLINE_2D)
         outputs, _ = torch.max(outputs, dim=0)
+        if single_class_mode:
+            outputs = torch.sigmoid(outputs)
         outputs = outputs.permute(1, 2, 0)
         outputs = outputs.reshape(padded, padded, num_classes).cpu().numpy().astype('float16')
         outputs = outputs[dist_samples:-dist_samples, dist_samples:-dist_samples, :]
@@ -284,7 +293,11 @@ def segmentation(param,
                          total=total_inf_windows,
                          desc="Writing to array"):
         arr1 = fp[row:row + chunk_size, col:col + chunk_size, :] / (2 ** 2)
-        arr1 = arr1.argmax(axis=-1).astype('uint8')
+        if single_class_mode:
+            arr1 = (arr1 > threshold)
+            arr1 = np.squeeze(arr1, axis=2).astype(np.uint8)
+        else:
+            arr1 = arr1.argmax(axis=-1).astype('uint8')
         pred_img[row:row + chunk_size, col:col + chunk_size] = arr1
     pred_img = pred_img[:h, :w]
     end_seg = time.time() - start_seg
@@ -561,10 +574,11 @@ def main(params: dict) -> None:
         device = torch.device('cpu')
 
     # CONFIGURE MODEL
-    num_classes_backgr = add_background_to_num_class(task, num_classes)
+    if num_classes > 1:
+        num_classes = num_classes + 1  # multiclass account for background
     model, loaded_checkpoint, model_name = net(model_name=model_name,
                                                num_bands=num_bands,
-                                               num_channels=num_classes_backgr,
+                                               num_channels=num_classes,
                                                dontcare_val=dontcare_val,
                                                num_devices=1,
                                                net_params=params,
@@ -675,7 +689,7 @@ def main(params: dict) -> None:
             pred, gdf = segmentation(param=params,
                                      input_image=raster,
                                      label_arr=label,
-                                     num_classes=num_classes_backgr,
+                                     num_classes=num_classes,
                                      gpkg_name=gpkg_name,
                                      model=model,
                                      chunk_size=chunk_size,
@@ -688,7 +702,7 @@ def main(params: dict) -> None:
                 gdf_.append(gdf)
                 gpkg_name_.append(gpkg_name)
             if local_gpkg and 'tracker_uri' in locals():
-                pixelMetrics = ComputePixelMetrics(label, pred, num_classes_backgr)
+                pixelMetrics = ComputePixelMetrics(label, pred, num_classes)
                 log_metrics(pixelMetrics.update(pixelMetrics.iou))
                 log_metrics(pixelMetrics.update(pixelMetrics.dice))
             pred = pred[np.newaxis, :, :].astype(np.uint8)
