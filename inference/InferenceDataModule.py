76cd4bb2c6c27303f792e3ee945d2094a8b371d2
remtav <remitavon@gmail.com>
2022-03-29 16:44:51 -0400

    - adapt inference and postprocessing to new single-class models inference_segmentation.py: - use hydra to instantiate test-time augmentations - finish documentation - read single class mode from checkpoint not config - add error handling if output name has an extension InferenceDataset.py: make input bands optional postprocess_segmentation.py: - adapt confidence value writing to new single-class extractions - finish documentation - fix bug if chaining inference and postprocessing for non-building models - get heatmap path from InferenceDataset.py - add command validation when using regularized output as polygonization input utils.py: further adapt upgrade_gdl_checkpoint for old single-class models InferenceDataModule.py: softcode test transforms (pad, enhance, etc.)

--- inference/InferenceDataModule.py
+++ inference/InferenceDataModule.py
@@ -16,6 +16,73 @@ from inference.GridGeoSamplerPlus import GridGeoSamplerPlus
 from inference.InferenceDataset import InferenceDataset
 
 
+# adapted from: https://github.com/microsoft/torchgeo/blob/3f7e525fbd01dddd25804e7a1b7634269ead1760/torchgeo/datamodules/chesapeake.py#L100
+def pad(
+        size: int = 512, mode='constant'
+) -> Callable[[Dict[str, Tensor]], Dict[str, Tensor]]:
+    """Returns a function to perform a padding transform on a single sample.
+    Args:
+        size: size of padding to apply
+        image_value: value to pad image with
+        mask_value: value to pad mask with
+    Returns:
+        function to perform padding
+    """
+    # use _pad from utils
+    def _pad(sample: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        """ Pads img_arr """
+        sample["image"] = F.pad(sample["image"], (size, size, size, size), mode=mode)
+        return sample
+
+    return _pad
+
+
+def enhance(
+        clip_limit=0.1
+) -> Callable[[Dict[str, Tensor]], Dict[str, Tensor]]:
+    """
+    Returns a function to perform a histogram streching (aka enhancement) on a single sample.
+    """
+    def _enhance(sample: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        """
+        Clip histogram by clip_limit # FIXME apply to entire image before inference? apply according to class?
+        @param sample: sample dictionary containing image
+        @param clip_limit:
+        @return:
+        """
+        sample['image'] = np.moveaxis(sample["image"].numpy().astype(np.uint8), 0, -1)  # send channels last
+        img_adapteq = []
+        for band in range(sample['image'].shape[-1]):
+            out_band = exposure.equalize_adapthist(sample["image"][..., band], clip_limit=clip_limit)
+            out_band = (out_band*255).astype(int)
+            img_adapteq.append(out_band)
+        out_stacked = np.stack(img_adapteq, axis=-1)
+        sample["image"] = torch.from_numpy(np.moveaxis(out_stacked, -1, 0))
+        return sample
+    return _enhance
+
+
+def preprocess(
+) -> Callable[[Dict[str, Tensor]], Dict[str, Tensor]]:
+    """
+    Returns a function to preprocess a single sample.
+    @param sample:
+    @return:
+    """
+    def _preprocess(sample: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        """Preprocesses a single sample.
+        Args:
+            sample: sample dictionary containing image
+        Returns:
+            preprocessed sample
+        """
+        sample["image"] = sample["image"] / 255.0
+        sample["image"] = sample["image"].float()
+
+        return sample
+    return _preprocess
+
+
 # adapted from https://github.com/microsoft/torchgeo/blob/3f7e525fbd01dddd25804e7a1b7634269ead1760/torchgeo/datamodules/chesapeake.py
 class InferenceDataModule(LightningDataModule):
     """LightningDataModule implementation for the InferenceDataset.
@@ -39,14 +106,28 @@ class InferenceDataModule(LightningDataModule):
         **kwargs: Any,
     ) -> None:
         """Initialize a LightningDataModule for InferenceDataset based Dataloader.
-        Args:
-            root_dir: The ``root`` argugment to pass to the InferenceDataset class
-            patch_size: The size of each patch in pixels
-            batch_size: The batch size to use in all created DataLoaders
-            num_workers: The number of workers to use in all created DataLoaders
-            download: TODO
-            use_projection_units : bool, optional
+        @param item_path:
+            path to stac item containing imagery assets to infer on
+        @param root_dir:
+            The ``root`` argugment to pass to the InferenceDataset class
+        @param outpath:
+            path to desired output
+        @param patch_size:
+            The size of each patch in pixels
+        @param stride:
+            stride between each chip
+        @param pad:
+            padding to apply to each chip
+        @param batch_size:
+            The batch size to use in all created DataLoaders
+        @param num_workers:
+            The number of workers to use in all created DataLoaders
+        @param download:
+            if True, download dataset and store it in the root directory.
+        @param use_projection_units : bool, optional
             Is `patch_size` in pixel units (default) or distance units?
+        @param save_heatmap: bool, optional
+            if True, saves heatmap from raw inference, after merging and smoothing chips
         Raises:
             ValueError: if ``use_prior_labels`` is used with ``class_set==7``
         """
@@ -65,58 +146,6 @@ class InferenceDataModule(LightningDataModule):
         self.bands = bands
         self.save_heatmap = save_heatmap
 
-    # adapted from: https://github.com/microsoft/torchgeo/blob/3f7e525fbd01dddd25804e7a1b7634269ead1760/torchgeo/datamodules/chesapeake.py#L100
-    @staticmethod
-    def pad(
-        size: int = 512, mode='constant'
-    ) -> Callable[[Dict[str, Tensor]], Dict[str, Tensor]]:
-        """Returns a function to perform a padding transform on a single sample.
-        Args:
-            size: size of padding to apply
-            image_value: value to pad image with
-            mask_value: value to pad mask with
-        Returns:
-            function to perform padding
-        """
-        # use _pad from utils
-        def _pad(sample: Dict[str, Tensor]) -> Dict[str, Tensor]:
-            """ Pads img_arr """
-            sample["image"] = F.pad(sample["image"], (size, size, size, size), mode=mode)
-            return sample
-
-        return _pad
-
-    @staticmethod
-    def enhance(sample: Dict[str, Any], clip_limit=0.1) -> Dict[str, Any]:
-        """
-        Clip histogram by clip_limit # FIXME apply to entire image before inference? apply according to class?
-        @param sample: sample dictionary containing image
-        @param clip_limit:
-        @return:
-        """
-        sample['image'] = np.moveaxis(sample["image"].numpy().astype(np.uint8), 0, -1)  # send channels last
-        img_adapteq = []
-        for band in range(sample['image'].shape[-1]):
-            out_band = exposure.equalize_adapthist(sample["image"][..., band], clip_limit=clip_limit)
-            out_band = (out_band*255).astype(int)
-            img_adapteq.append(out_band)
-        out_stacked = np.stack(img_adapteq, axis=-1)
-        sample["image"] = torch.from_numpy(np.moveaxis(out_stacked, -1, 0))
-        return sample
-
-    @staticmethod
-    def preprocess(sample: Dict[str, Any]) -> Dict[str, Any]:
-        """Preprocesses a single sample.
-        Args:
-            sample: sample dictionary containing image
-        Returns:
-            preprocessed sample
-        """
-        sample["image"] = sample["image"] / 255.0
-        sample["image"] = sample["image"].float()
-
-        return sample
-
     def prepare_data(self) -> None:
         """Confirms that the dataset is downloaded on the local node.
         This method is called once per node, while :func:`setup` is called once per GPU.
@@ -131,26 +160,14 @@ class InferenceDataModule(LightningDataModule):
             pad=self.pad_size,
         )
 
-    def setup(self, stage: Optional[str] = None):
+    def setup(self, stage: Optional[str] = None,
+              test_transforms: Compose = Compose([pad(16, mode='reflect'), enhance, preprocess])):
         """Instantiate the InferenceDataset.
         The splits should be done here vs. in :func:`__init__` per the docs:
         https://pytorch-lightning.readthedocs.io/en/latest/extensions/datamodules.html#setup.
-        Args:
-            stage: stage to set up
-        self.test_dataset = ChesapeakeCVPR(
-            self.root_dir,
-            splits=self.test_splits,
-            layers=self.layers,
-            transforms=test_transforms,
-            download=False,
-            checksum=False,
-        )
+        @param test_transforms:
+        @param stage: stage to set up
         """
-        test_transforms = Compose([self.pad(self.pad_size, mode='reflect'),
-                                   self.enhance,
-                                   self.preprocess,
-                                   ])
-
         self.inference_dataset = InferenceDataset(
             item_path=self.item_path,
             root=self.root_dir,
