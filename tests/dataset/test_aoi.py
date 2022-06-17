d035f3c406adb5f906764a7a91318f43f58fcac5
remtav <remitavon@gmail.com>
2022-06-17 16:52:38 -0400

    test_aoi.py: remove commented test for invalid label features (TODO)

--- tests/dataset/test_aoi.py
+++ tests/dataset/test_aoi.py
@@ -42,21 +42,6 @@ class Test_AOI(object):
             with pytest.raises(AttributeError):
                 aoi = AOI(raster=row['tif'], label=row['gpkg'], split=row['split'])
 
-
-    # TODO
-    # def test_invalid_label_feature(self):
-    #     dir = Path("/media/data/ccmeo_label/geopackage")
-    #     globbed = list(dir.glob("**/*.gpkg"))
-    #     for label in globbed:
-    #         try:
-    #             fionas, gpds = validate_features_from_gpkg(str(label), attribute_name="id")
-    #             if len(fionas) > 0 or len(gpds) > 0:
-    #                 print(fionas, gpds)
-    #
-    #         except AssertionError:
-    #             pass
-
-
     def test_parse_input_raster(self) -> None:
         raster_raw = {
             "tests/data/spacenet/SpaceNet_AOI_2_Las_Vegas-056155973080_01_P001-WV03.json": [
