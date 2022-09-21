ec8a8cad1083cfee2347557271669eea735dfa6a
Mathieu <mathieu.turgeon-pelchat@canada.ca>
2022-09-21 12:46:50 -0400

    fix tests for init test tensors

--- tests/utils/test_metrics.py
+++ tests/utils/test_metrics.py
@@ -1,20 +1,23 @@
-import numpy as np
-from torchmetrics.functional import precision_recall
 from utils.metrics import create_metrics_dict, report_classification, iou
 import torch
-
-import segmentation_models_pytorch as smp
-from torchmetrics import JaccardIndex
+import pytest
 
 # Test arrays: [bs=2, h=2, w,2]
-pred_multi = torch.tensor([0, 0, 2, 2, 0, 2, 1, 2, 1, 0, 2, 2, 1, 0, 2, 2])
-pred_binary = torch.tensor([0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1])
+def init_tensors():
+    pred_multi = torch.tensor([0, 0, 2, 2, 0, 2, 1, 2, 1, 0, 2, 2, 1, 0, 2, 2])
+    pred_binary = torch.tensor([0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1])
 
-lbl_multi = torch.tensor([1, 0, 2, 2, 0, 1, 2, 0, 2, 2, 0, 0, 1, 2, 0, 1])
-lbl_binary = torch.tensor([1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1])
-# array with dont care
-lbl_multi_dc = torch.tensor([-1, -1, 2, 2, 0, 1, 2, 0, 2, 2, 0, 0, 1, 2, 0, 1])
-lbl_binary_dc = torch.tensor([-1, -1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1])
+    lbl_multi = torch.tensor([1, 0, 2, 2, 0, 1, 2, 0, 2, 2, 0, 0, 1, 2, 0, 1])
+    lbl_binary = torch.tensor([1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1])
+    # array with dont care
+    lbl_multi_dc = torch.tensor([-1, -1, 2, 2, 0, 1, 2, 0, 2, 2, 0, 0, 1, 2, 0, 1])
+    lbl_binary_dc = torch.tensor([-1, -1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1])
+    return {'pred_multi': pred_multi, 
+            'pred_binary': pred_binary, 
+            'lbl_multi': lbl_multi, 
+            'lbl_binary': lbl_binary, 
+            'lbl_multi_dc': lbl_multi_dc, 
+            'lbl_binary_dc': lbl_binary_dc}
 
 
 class TestMetrics(object):
@@ -37,9 +40,10 @@ class TestMetrics(object):
     def test_report_classification_multi(self):
         """Evaluate report classification. 
         Multiclass, without ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
-        metrics_dict = report_classification(pred_multi, 
-                                             lbl_multi, 
+        metrics_dict = report_classification(t['pred_multi'], 
+                                             t['lbl_multi'], 
                                              batch_size=2, 
                                              metrics_dict=metrics_dict,
                                              ignore_index=-1)
@@ -50,9 +54,10 @@ class TestMetrics(object):
     def test_report_classification_multi_ignore_idx(self):
         """Evaluate report classification. 
         Multiclass, with ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
-        metrics_dict = report_classification(pred_multi, 
-                                             lbl_multi_dc, 
+        metrics_dict = report_classification(t['pred_multi'], 
+                                             t['lbl_multi_dc'], 
                                              batch_size=2, 
                                              metrics_dict=metrics_dict,
                                              ignore_index=-1)
@@ -63,9 +68,10 @@ class TestMetrics(object):
     def test_report_classification_binary(self):
         """Evaluate report classification. 
         Binary, without ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(1)
-        metrics_dict = report_classification(pred_binary, 
-                                             lbl_binary, 
+        metrics_dict = report_classification(t['pred_binary'], 
+                                             t['lbl_binary'], 
                                              batch_size=2, 
                                              metrics_dict=metrics_dict,
                                              ignore_index=-1)
@@ -76,9 +82,10 @@ class TestMetrics(object):
     def test_report_classification_binary_ignore_idx(self):
         """Evaluate report classification. 
         Binary, without ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(1)
-        metrics_dict = report_classification(pred_binary, 
-                                             lbl_binary_dc, 
+        metrics_dict = report_classification(t['pred_binary'], 
+                                             t['lbl_binary_dc'], 
                                              batch_size=2, 
                                              metrics_dict=metrics_dict,
                                              ignore_index=-1)
@@ -89,9 +96,10 @@ class TestMetrics(object):
     def test_iou_multi(self):
         """Evaluate iou calculation. 
         Multiclass, without ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
-        metrics_dict = iou(pred_multi, 
-                           lbl_multi, 
+        metrics_dict = iou(t['pred_multi'], 
+                           t['lbl_multi'], 
                            batch_size=2, 
                            num_classes=3,
                            metric_dict=metrics_dict,
@@ -101,10 +109,11 @@ class TestMetrics(object):
     def test_iou_multi_ignore_idx(self):
         """Evaluate iou calculation. 
         Multiclass, with ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
         # wih ignore_index == -1
-        metrics_dict = iou(pred_multi, 
-                           lbl_multi_dc, 
+        metrics_dict = iou(t['pred_multi'], 
+                           t['lbl_multi_dc'], 
                            batch_size=2, 
                            num_classes=3,
                            metric_dict=metrics_dict,
@@ -112,9 +121,10 @@ class TestMetrics(object):
         assert "{:.6f}".format(metrics_dict['iou'].val) == "0.233333"
 
         # with ignore_index == 0
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
-        metrics_dict = iou(pred_multi, 
-                           lbl_multi, 
+        metrics_dict = iou(t['pred_multi'], 
+                           t['lbl_multi'], 
                            batch_size=2, 
                            num_classes=3,
                            metric_dict=metrics_dict,
@@ -124,9 +134,10 @@ class TestMetrics(object):
     def test_iou_binary(self):
         """Evaluate iou calculation. 
         Binary, without ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(1)
-        metrics_dict = iou(pred_binary, 
-                           lbl_binary, 
+        metrics_dict = iou(t['pred_binary'], 
+                           t['lbl_binary'], 
                            batch_size=2, 
                            num_classes=1,
                            metric_dict=metrics_dict,
@@ -136,10 +147,11 @@ class TestMetrics(object):
     def test_iou_binary_ignore_idx(self):
         """Evaluate iou calculation. 
         Binary, with ignore_index in array."""
+        t = init_tensors()
         metrics_dict = create_metrics_dict(1)
         # with ignore_index == -1
-        metrics_dict = iou(pred_binary, 
-                           lbl_binary_dc, 
+        metrics_dict = iou(t['pred_binary'], 
+                           t['lbl_binary_dc'], 
                            batch_size=2, 
                            num_classes=1,
                            metric_dict=metrics_dict,
@@ -147,11 +159,12 @@ class TestMetrics(object):
         assert "{:.6f}".format(metrics_dict['iou'].val) == "0.435897"
 
         # with ignore_index == 0
+        t = init_tensors()
         metrics_dict = create_metrics_dict(3)
-        metrics_dict = iou(pred_multi, 
-                           lbl_multi, 
-                           batch_size=2, 
-                           num_classes=3,
-                           metric_dict=metrics_dict,
-                           ignore_index=0)
-        assert "{:.6f}".format(metrics_dict['iou'].val) == "0.208333"
+        with pytest.raises(ValueError):
+            metrics_dict =  iou(t['pred_binary'], 
+                                t['lbl_binary_dc'], 
+                                batch_size=2, 
+                                num_classes=3,
+                                metric_dict=metrics_dict,
+                                ignore_index=0)
