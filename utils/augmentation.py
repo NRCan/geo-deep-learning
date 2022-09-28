dbaad9e709b4659e1316bc4d92c3a0294e8facfa
remtav <remitavon@gmail.com>
2022-09-28 16:37:00 -0400

    augmentation.py: remove BGR_to_RGB utilities

--- utils/augmentation.py
+++ utils/augmentation.py
@@ -10,16 +10,15 @@ import torch
 # Unclear issue, mentioned here: https://github.com/pytorch/pytorch/issues/2083
 import random
 import numpy as np
-from skimage import transform, exposure
+from skimage import transform
 from torchvision import transforms
-from utils.utils import get_key_def, pad, minmax_scale, BGR_to_RGB
+from utils.utils import get_key_def, pad, minmax_scale
 
 logging.getLogger(__name__)
 
 
 def compose_transforms(params,
                        dataset,
-                       input_space: bool = False,
                        scale: Sequence = None,
                        aug_type: str = '',
                        dontcare=None,
@@ -28,7 +27,6 @@ def compose_transforms(params,
                        print_log=True):
     """
     Function to compose the transformations to be applied on every batches.
-    :param input_space: (bool) if True, flip BGR channels to RGB
     :param params: (dict) Parameters found in the yaml config file
     :param dataset: (str) One of 'trn', 'val', 'tst'
     :param aug_type: (str) One of 'geometric', 'radiometric'
@@ -69,15 +67,6 @@ def compose_transforms(params,
                 lst_trans.append(RandomCrop(sample_size=crop_size, ignore_index=dontcare))
 
     if aug_type == 'totensor':
-        if input_space:
-            lst_trans.append(BgrToRgb(input_space))
-        else:
-            if print_log:
-                logging.info(
-                    f"\nThe '{dataset}' images will be fed to model as is. "
-                    f'First 3 bands of imagery should be RGB, not BGR.'
-                )
-
         if scale:
             lst_trans.append(Scale(scale))  # TODO: assert coherence with below normalization
         else:
@@ -284,19 +273,6 @@ class Normalize(object):
             return sample
         else:
             return sample
-
-
-class BgrToRgb(object):
-    """Normalize Image with Mean and STD and similar to Pytorch(transform.Normalize) function """
-
-    def __init__(self, bgr_to_rgb):
-        self.bgr_to_rgb = bgr_to_rgb
-
-    def __call__(self, sample):
-        sat_img = BGR_to_RGB(sample['sat_img']) if self.bgr_to_rgb else sample['sat_img']
-        sample['sat_img'] = sat_img
-
-        return sample
 
 
 class ToTensorTarget(object):
