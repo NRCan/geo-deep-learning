7860313a752a2471ccc3afd26e923556a2c1494d
CharlesAuthier <charles.authier93@gmail.com>
2023-03-08 13:58:13 -0500

    try to get error

--- tests/loss/test_losses.py
+++ tests/loss/test_losses.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import torch
+import os
 from hydra import initialize, compose
 from hydra.core.hydra_config import HydraConfig
 from hydra.utils import to_absolute_path
@@ -15,7 +16,7 @@ class TestLossesZoo(object):
             for dataset_type in ['binary', 'multiclass']:
                 num_classes = 1 if dataset_type == 'binary' else 5
                 class_weights = [1/num_classes for i in range(num_classes)]
-                print(Path(to_absolute_path(f"../../config/loss/{dataset_type}")).glob('*.yaml'))
+                print(os.listdir(to_absolute_path(f"../../config/loss/{dataset_type}")))
                 #verify_weights(num_classes, class_weights)
                 for loss_config in Path(to_absolute_path(f"../../config/loss/{dataset_type}")).glob('*.yaml'):
                     print(loss_config)
@@ -35,23 +36,9 @@ class TestLossesZoo(object):
                     print(loss)
                     loss.backward()
     
-    # def test_define_loss(self) -> None:
-    #     with initialize(config_path="../../config", job_name="test_ci"):
-    #         for dataset_type in ['binary', 'multiclass']:
-    #             num_classes = 1 if dataset_type == 'binary' else 5
-    #             class_weights = [1/num_classes for i in range(num_classes)]
-    #             verify_weights(num_classes, class_weights)
-    #             for loss_config in Path(to_absolute_path(f"../../config/loss/{dataset_type}")).glob('*.yaml'):
-    #                 cfg = compose(config_name="gdl_config_template",
-    #                               overrides=[f"loss={dataset_type}/{loss_config.stem}"],
-    #                               return_hydra_config=True)
-    #                 hconf = HydraConfig()
-    #                 hconf.set_config(cfg)
-    #                 del cfg.loss.is_binary  # prevent exception at instantiation
-    #                 criterion = define_loss(loss_params=cfg.loss, class_weights=class_weights)
-    #                 # test if binary and multiclass work
-    #                 outputs = torch.randn(1, num_classes, 256, 256, requires_grad=True)
-    #                 labels = torch.randn(1, 256, 256)
-    #                 loss = criterion(outputs, labels.unsqueeze(1).float())
-    #                 #loss = criterion(outputs, labels) if num_classes > 1 else criterion(outputs, labels.unsqueeze(1).float())
-    #                 loss.backward()
+    def test_binary_multi_loss(self) -> None:
+        loss = torch.nn.CrossEntropyLoss(ignore_index=255)
+        # Example of target with class probabilities
+        input = torch.ones(32, 1, 512, 512, requires_grad=True)
+        target = torch.ones(32, 515, 512, dtype=torch.long)
+        output = loss(input, target.unsqueeze(1).float())
