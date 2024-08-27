6cc3833638b0206c15336460217671eae017850d
valhassan <victor.alhassan@canada.ca>
2024-08-27 15:33:07 -0400

    Update SegmentationSegformer class with configurable loss and optimizer

--- geo-deep-learning/tasks_with_models/segmentation_segformer.py
+++ geo-deep-learning/tasks_with_models/segmentation_segformer.py
@@ -1,16 +1,19 @@
 import pytorch_lightning as pl
+from typing import Any, Callable
+from hydra.utils import instantiate
 from models.segformer import SegFormer
-from ..tools.losses.segmentation_losses import *
 
 class SegmentationSegformer(pl.LightningModule):
     def __init__(self, 
                  encoder: str,
                  in_channels: int, 
                  num_classes: int,
-                 loss: str = 'cross_entropy'):
+                 loss: Callable, 
+                 **kwargs: Any):
         super().__init__()
         self.save_hyperparameters()
         self.model = SegFormer(encoder, in_channels, num_classes)
+        self.loss = loss
 
     def forward(self, x):
         return self.model(x)
@@ -18,20 +21,29 @@ class SegmentationSegformer(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
-        loss = F.cross_entropy(y_hat, y)
+        loss = self.loss(y_hat, y)
         self.log('train_loss', loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.model(x)
-        loss = F.cross_entropy(y_hat, y)
+        loss = self.loss(y_hat, y)
         self.log('val_loss', loss)
         return loss
-
+    
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-4)
-        return optimizer
+        optimizer = instantiate(self.hparams["optimizer"], params=self.model.parameters())
+        scheduler = instantiate(self.hparams["lr_scheduler"])
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "epoch",
+                "monitor": "val_loss"
+                }
+        }
+    
 
     def train_dataloader(self):
         return DataLoader(train_dataset, batch_size=32, num_workers=4, shuffle=True)
