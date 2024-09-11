a938984fd128e04f868a10fce05619e54c628d46
valhassan <victor.alhassan@canada.ca>
2024-09-11 14:06:16 -0400

    Update metrics in SegmentationSegformer

--- geo_deep_learning/tasks_with_models/segmentation_segformer.py
+++ geo_deep_learning/tasks_with_models/segmentation_segformer.py
@@ -1,7 +1,8 @@
+import numpy as np
 from torch import Tensor
 from typing import Any, Callable, Dict, List
 from lightning.pytorch import LightningModule
-from torchmetrics.segmentation import MeanIoU
+from torchmetrics.classification import MulticlassJaccardIndex
 from torchmetrics.wrappers import ClasswiseWrapper
 from models.segformer import SegFormer
 
@@ -17,7 +18,7 @@ class SegmentationSegformer(LightningModule):
         self.save_hyperparameters()
         self.model = SegFormer(encoder, in_channels, num_classes)
         self.loss = loss
-        self.metric = MeanIoU(num_classes=num_classes, per_class=True)
+        self.metric= MulticlassJaccardIndex(num_classes=num_classes, average=None, zero_division=np.nan)
         self.labels = [str(i) for i in range(num_classes)] if class_labels is None else class_labels
         self.classwise_metric = ClasswiseWrapper(self.metric, labels=self.labels)
 
@@ -33,24 +34,23 @@ class SegmentationSegformer(LightningModule):
         y_hat = y_hat.argmax(dim=1)
         self.log('train_loss', loss, 
                  prog_bar=True, logger=True, 
-                 on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=0)
+                 on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x = batch["image"]
         y = batch["label"]
-        print(f"batch_index: {batch_idx},  x: {x.shape}, y: {y.shape}")
         y = y.squeeze(1).long()
         y_hat = self(x)
-        print(f"batch_index: {batch_idx},  y_hat: {y_hat.shape}, y: {y.shape}")
         loss = self.loss(y_hat, y)
         y_hat = y_hat.softmax(dim=1).argmax(dim=1)
-        print(f"batch_index: {batch_idx},  y_hat: {y_hat.shape}, y: {y.shape}")
-        val_metrics = self.classwise_metric(y_hat, y)
-        val_metrics["val_loss"] = loss
-        self.log_dict(val_metrics, 
-                      prog_bar=True, logger=True, 
-                      on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=0)
+        mean_iou = self.metric(y_hat, y)
+        self.log('val_loss', loss,
+                    prog_bar=True, logger=True, 
+                    on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=True)
+        # self.log_dict(val_metrics, 
+        #               prog_bar=True, logger=True, 
+        #               on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=0)
     
     def test_step(self, batch, batch_idx):
         x = batch["image"]
@@ -63,5 +63,5 @@ class SegmentationSegformer(LightningModule):
         test_metrics["test_loss"] = loss
         self.log_dict(test_metrics, 
                       prog_bar=True, logger=True, 
-                      on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=0)
+                      on_step=False, on_epoch=True, sync_dist=True, rank_zero_only=True)
     
