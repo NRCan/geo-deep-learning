c857057a0208af19edd558d1a8c4ecdf46d28519
valhassan <victor.alhassan@canada.ca>
2024-10-09 12:53:21 -0400

    added script_model.py

---
+++ geo_deep_learning/tools/script_model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class ScriptModel(nn.Module):
+    def __init__(self, model, data_module):
+        super().__init__()
+        self.model = model
+        self.data_module = data_module
+    
+    def forward(self, x):
+        sample = {"image": x}
+        preprocessed = self.data_module.preprocess(sample)
+        return self.model(preprocessed["image"])
+
+def script_model(model, data_module):
+    return ScriptModel(model, data_module)
+        
