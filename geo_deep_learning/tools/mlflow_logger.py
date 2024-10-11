7f2c5b09e85c2a026cee739e68743b8d696c5f63
valhassan <victor.alhassan@canada.ca>
2024-10-10 21:10:23 -0400

    cleanup

--- geo_deep_learning/tools/mlflow_logger.py
+++ geo_deep_learning/tools/mlflow_logger.py
@@ -6,11 +6,7 @@ from lightning.pytorch.loggers import MLFlowLogger
 class LoggerSaveConfigCallback(SaveConfigCallback):
     def save_config(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
         if isinstance(trainer.logger, MLFlowLogger):
-            print("Logging Config file to MLFlow.")
-            # config = self.parser.dump(self.config, skip_none=False)  # Required for proper reproducibility
-            # print(self.config.config[0])
             config_filepath = self.config.config[0]
-            print(type(config_filepath))
             trainer.logger.experiment.log_artifact(local_path=config_filepath, 
                                                    artifact_path="config",
                                                    run_id=trainer.logger.run_id)
