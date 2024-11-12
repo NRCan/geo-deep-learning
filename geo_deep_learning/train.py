48e0244efda249534cf61b1720eb0900ed91b9dd
valhassan <victor.alhassan@canada.ca>
2024-11-12 13:44:21 -0500

    parser mode enabled to omegaconf, test logger

--- geo_deep_learning/train.py
+++ geo_deep_learning/train.py
@@ -1,7 +1,14 @@
 from tools.mlflow_logger import LoggerSaveConfigCallback
 from lightning.pytorch import Trainer
+from lightning.pytorch.loggers import MLFlowLogger
 from lightning.pytorch.cli import ArgsType, LightningCLI
 
+class TestMLFlowLogger(MLFlowLogger):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    def log_hyperparams(self, params):
+        # Override to prevent hyperparameter logging during test
+        pass
 class GeoDeepLearningCLI(LightningCLI):
     def before_fit(self):
         self.datamodule.prepare_data()
@@ -11,14 +18,27 @@ class GeoDeepLearningCLI(LightningCLI):
     def after_fit(self):
         if self.trainer.is_global_zero:
             best_model_path = self.trainer.checkpoint_callback.best_model_path
+            
+            test_logger = TestMLFlowLogger(
+                experiment_name=self.trainer.logger._experiment_name,
+                run_name=self.trainer.logger._run_name,
+                run_id=self.trainer.logger.run_id,
+                save_dir=self.trainer.logger.save_dir
+            )
+            
             test_trainer = Trainer(devices=1, 
                                    accelerator="auto", 
                                    strategy="auto",
-                                   logger=False)
+                                   logger=test_logger,
+                                   )
+            
             best_model = self.model.__class__.load_from_checkpoint(best_model_path)
-            test_results = test_trainer.test(model=best_model, dataloaders=self.datamodule.test_dataloader())
+            test_results = test_trainer.test(model=best_model, 
+                                             dataloaders=self.datamodule.test_dataloader())
+            test_metrics = {}
             for metric_name, metric_value in test_results[0].items():
-                self.trainer.logger.log_metrics({f"test_{metric_name}": metric_value})
+                test_metrics[f"test_{metric_name}"] = metric_value
+                self.trainer.logger.log_metrics(test_metrics)
             self.trainer.logger.log_hyperparams({"best_model_path": best_model_path})
             print("Test metrics logged successfully to all loggers.")
         self.trainer.strategy.barrier()
@@ -41,12 +61,13 @@ class GeoDeepLearningCLI(LightningCLI):
             print(f"Number of training samples: {train_size}")
             print(f"Number of validation samples: {val_size}")
             print(f"Number of test samples: {test_size}")
-    
 
 
 def main(args: ArgsType = None) -> None:
     cli = GeoDeepLearningCLI(save_config_callback=LoggerSaveConfigCallback,
-                             save_config_kwargs={"overwrite": True},args=args)
+                             save_config_kwargs={"overwrite": True},
+                             parser_kwargs={"parser_mode": "omegaconf"},
+                             args=args)
     if cli.trainer.is_global_zero:
         print("Done!")
     
