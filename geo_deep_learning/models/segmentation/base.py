9aaf49ebf59b73a9d88141b779f79b28ae3d0d83
valhassan <victor.alhassan@canada.ca>
2025-02-20 20:48:28 -0500

    Refactor BaseSegmentationModel to use output_struct for flexible model outputs

--- geo_deep_learning/models/segmentation/base.py
+++ geo_deep_learning/models/segmentation/base.py
@@ -1,25 +1,24 @@
 import torch.nn as nn
 
 class BaseSegmentationModel(nn.Module):
-    def __init__(self, encoder, neck, decoder, head, auxilary_head=None):
+    def __init__(self, encoder, neck, decoder, head, output_struct, auxilary_head=None):
         super().__init__()
         self.encoder = encoder
         self.neck = neck
         self.decoder = decoder
         self.auxilary_head = auxilary_head
         self.head = head
-        
+        self.output_struct = output_struct
     def forward(self, x):
-        outputs = {}
         x = self.encoder(x)
         x = self.neck(x)
         x = self.decoder(x)
+        aux = None
         if self.auxilary_head:
             aux = self.auxilary_head(x)
-            outputs['aux'] = aux
         x = self.head(x)
-        outputs['out'] = x
-        return outputs
+        output = self.output_struct(out=x, aux=aux)
+        return output
     
     def _freeze_layers(self, layers):
         for name, param in self.named_parameters():
