58eece575b71f98e144a517461d732fcb8609106
valhassan <victor.alhassan@canada.ca>
2025-07-03 17:03:28 -0400

    Refactor MLFlow logger to improve code readability and add docstrings. Enhanced the save_config method with clearer formatting and documentation for better understanding of its functionality.

--- geo_deep_learning/tools/mlflow_logger.py
+++ geo_deep_learning/tools/mlflow_logger.py
@@ -1,12 +1,24 @@
-from lightning.pytorch import LightningModule
-from lightning.pytorch import Trainer
+"""MLFlow logger."""
+
+from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.cli import SaveConfigCallback
 from lightning.pytorch.loggers import MLFlowLogger
 
+
 class LoggerSaveConfigCallback(SaveConfigCallback):
-    def save_config(self, trainer: Trainer, pl_module: LightningModule, stage: str) -> None:
+    """Save config callback for MLFlow logger."""
+
+    def save_config(
+        self,
+        trainer: Trainer,
+        pl_module: LightningModule,  # noqa: ARG002
+        stage: str,  # noqa: ARG002
+    ) -> None:
+        """Save config."""
         if isinstance(trainer.logger, MLFlowLogger):
             config_filepath = self.config.config[0]
-            trainer.logger.experiment.log_artifact(local_path=config_filepath, 
-                                                   artifact_path="config",
-                                                   run_id=trainer.logger.run_id)
+            trainer.logger.experiment.log_artifact(
+                local_path=config_filepath,
+                artifact_path="config",
+                run_id=trainer.logger.run_id,
+            )
