7bcf096cc46c37d3c831ebe685d93b2b07b45060
valhassan <victor.alhassan@canada.ca>
2025-07-14 22:31:44 -0400

    Refactor DOFASegmentationModel to include wavelengths parameter in forward method, update encoder initialization with embed_dim settings, and enhance model structure with additional configuration options for neck and decoder components.

--- geo_deep_learning/models/segmentation/dofa.py
+++ geo_deep_learning/models/segmentation/dofa.py
@@ -2,12 +2,20 @@
 
 import torch
 import torch.nn.functional as fn
-from models.decoders.upernet import UperNetDecoder
-from models.encoders.dofa import DOFA, create_dofa_base, create_dofa_large
-from models.heads.fcn_head import FCNHead
-from models.heads.segmentation_head import SegmentationHead, SegmentationOutput
-from models.necks.multilevel_neck import MultiLevelNeck
 from torch import Tensor
+
+from geo_deep_learning.models.decoders.upernet import UperNetDecoder
+from geo_deep_learning.models.encoders.dofa import (
+    DOFA,
+    create_dofa_base,
+    create_dofa_large,
+)
+from geo_deep_learning.models.heads.fcn_head import FCNHead
+from geo_deep_learning.models.heads.segmentation_head import (
+    SegmentationHead,
+    SegmentationOutput,
+)
+from geo_deep_learning.models.necks.multilevel_neck import MultiLevelNeck
 
 from .base import BaseSegmentationModel
 
@@ -34,15 +42,22 @@ class DOFASegmentationModel(BaseSegmentationModel):
         )
 
         if encoder == "dofa_base":
+            self.embed_dim = 768
             self.encoder = create_dofa_base(img_size=image_size, pretrained=pretrained)
 
         elif encoder == "dofa_large":
+            self.embed_dim = 1024
             self.encoder = create_dofa_large(img_size=image_size, pretrained=pretrained)
+        else:
+            msg = f"Invalid encoder: {encoder}"
+            raise ValueError(msg)
 
         self.neck = MultiLevelNeck(
             in_channels=[self.embed_dim] * 4,
             out_channels=[self.embed_dim] * 4,
             scales=[4, 2, 1, 0.5],
+            norm_cfg={"type": "BN"},
+            act_cfg={"type": "ReLU"},
         )
         self.decoder = UperNetDecoder(
             embed_dim=[self.embed_dim] * 4,
@@ -64,10 +79,10 @@ class DOFASegmentationModel(BaseSegmentationModel):
         if freeze_layers:
             self._freeze_layers(layers=freeze_layers)
 
-    def forward(self, x: Tensor) -> SegmentationOutput:
+    def forward(self, x: Tensor, wavelengths: Tensor) -> SegmentationOutput:
         """Forward pass."""
         image_size = x.shape[2:]
-        x = self.encoder(x)
+        x = self.encoder(x, wavelengths)
         feats = self.neck(x)
         x = self.decoder(feats)
         x = self.head(x)
@@ -94,6 +109,7 @@ class DOFASegmentationModel(BaseSegmentationModel):
 if __name__ == "__main__":
     model = DOFASegmentationModel()
     x = torch.randn(5, 3, 512, 512)
-    outputs = model(x)
+    wavelengths = torch.randn(5, 3)
+    outputs = model(x, wavelengths)
     # print(f"outputs.shape: {outputs.out.shape}")
     # print(f"aux_outputs.shape: {outputs.aux.shape}")
